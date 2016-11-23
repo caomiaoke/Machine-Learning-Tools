0d8b23610e3d95eadea287d22f5b5ea62fc921c5
caomiaoke <caomiaoke@163.com>
2016-11-23 23:25:44 +0800

    Create LDA file and Math

---
+++ src/main/java/com/github/milo/classification/LDA.java
@@ -0,0 +1,8 @@
+package com.github.milo.classification;
+
+/**
+ * Created by milo on 16-11-23.
+ * Linear discriminant analysis
+ */
+public class LDA {
+}
---
+++ src/main/java/com/github/milo/math/Math.java
@@ -0,0 +1,7 @@
+package com.github.milo.math;
+
+/**
+ * Created by milo on 16-11-23.
+ */
+public class Math {
+}
