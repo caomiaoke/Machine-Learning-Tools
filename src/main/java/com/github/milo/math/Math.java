943c1a363a24793e63cc403b3f4587302b85192e
caomiaoke <caomiaoke@163.com>
2016-11-25 00:16:07 +0800

    Add the abs and unique function

--- src/main/java/com/github/milo/math/Math.java
+++ src/main/java/com/github/milo/math/Math.java
@@ -1,7 +1,39 @@
 package com.github.milo.math;
 
+import java.util.HashSet;
+import java.util.Iterator;
+
 /**
  * Created by milo on 16-11-23.
  */
 public class Math {
+
+
+    /**
+     * private constructor
+     */
+
+    private Math(){}
+
+    public static double abs(double a ){
+        return java.lang.Math.abs(a);
+    }
+    /**
+     * 向量值唯一化
+     */
+    public static int[] unique(int[] x){
+        HashSet<Integer> hash = new HashSet<Integer>();
+        for (int i = 0; i < x.length; i++){
+            hash.add(x[i]);
+        }
+
+
+        int[] y = new int[hash.size()];
+
+        Iterator<Integer> iterator = hash.iterator();
+        for (int i = 0; i < y.length;i++){
+            y[i] = iterator.next();
+        }
+        return y;
+    }
 }
