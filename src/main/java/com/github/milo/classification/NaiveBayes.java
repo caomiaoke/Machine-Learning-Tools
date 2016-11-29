490f6d0f517b4fcd29001e1a0e70e594703adcba
caomiaoke <caomiaoke@163.com>
2016-11-29 09:46:38 +0800

    Create OnlineClassifier file

---
+++ src/main/java/com/github/milo/classification/NaiveBayes.java
@@ -0,0 +1,11 @@
+package com.github.milo.classification;
+
+import java.io.Serializable;
+
+/**
+ * Created by Miaoke on 2016/11/28.
+ */
+public class NaiveBayes implements OnlineClassifier<double[]>,SoftClassifier<double[]>,Serializable{
+    private static final long serialVersionUID = 1L;
+
+}
